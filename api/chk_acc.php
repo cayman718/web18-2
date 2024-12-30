b19abeaab044c47158c77556a7d5fb387fd5ae8f
cayman718 <a44362652@gmail.com>
2024-12-30 16:26:47 +0800

    乙級第二題註冊功能

---
+++ api/chk_acc.php
@@ -0,0 +1,18 @@
+<?php include_once "db.php";
+
+// $acc=$_GET['acc'];
+// $User->count(['acc'=>$acc]);
+
+echo $User->count($_GET);
+
+/* 
+if($res>0){
+    echo 1;
+}else{
+    echo 0;
+} */
+
+
+
+
+?>
